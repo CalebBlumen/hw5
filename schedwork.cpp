c3a475fc55fe123c6dd261913c80767de5015d04
Caleb Blumenfeld <cblumenf@usc.edu>
2024-04-18 02:29:37 +0000

    hw5 completed

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,39 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+void testPrintSchedule(const DailySchedule& sched)
+{
+    int day = 0;
+    for(auto s : sched)
+    {
+        cout << "Day " << day << ": ";
+        for(auto nurse : s)
+        {
+            cout << nurse << " ";
+        }
+        cout << endl;
+        day++;
+    }
+}
+
+bool isValid(
+    const AvailabilityMatrix& avail,
+    const vector<unsigned int>& shifts,
+    const size_t maxShifts,
+    const DailySchedule& sched,
+    const unsigned int currWorker,
+    const unsigned int day
+);
+
+bool scheduleRec(
+    const AvailabilityMatrix& avail,
+    vector<unsigned int>& shifts,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    unsigned int r,
+    unsigned int c
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +70,70 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    unsigned int numWorkers = avail[0].size();
+    unsigned int d = avail.size(); //number of days
 
+    
+    vector<unsigned int> empties;
+    for (unsigned int i = 0; i < dailyNeed; ++i) {
+      empties.push_back(numWorkers);
+    }
 
+    for (unsigned int i = 0; i < d; ++i) {
+      sched.push_back(empties);
+    }
+    vector<unsigned int> shifts(numWorkers);
 
+    return scheduleRec(avail,shifts,dailyNeed,maxShifts,sched,0,0);
 
+}
+
+bool scheduleRec(
+    const AvailabilityMatrix& avail,
+    vector<unsigned int>& shifts,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    unsigned int r,
+    unsigned int c
+)
+{
+  if (r == avail.size()) {
+    return true;
+  }
+  for (unsigned int i = 0; i < avail[0].size(); ++i) {
+    sched[r][c] = i;
+    shifts[i]++;
+    
+    if (isValid(avail,shifts,maxShifts,sched,i,r)) {
+      bool status;
+      if (c < sched[0].size()-1) {
+        status = scheduleRec(avail, shifts, dailyNeed, maxShifts, sched, r, c+1);
+      }
+      else {
+        status = scheduleRec(avail, shifts, dailyNeed, maxShifts, sched, r+1, 0);
+      }
+      
+      if (status) return true;
+    }
+    sched[r][c] = avail[0].size();
+    shifts[i]--;
+  }
+  return false;
+}
+
+bool isValid(
+    const AvailabilityMatrix& avail,
+    const vector<unsigned int>& shifts,
+    const size_t maxShifts,
+    const DailySchedule& sched,
+    const unsigned int currWorker,
+    const unsigned int day
+)
+{
+  if (shifts[currWorker] > maxShifts || !avail[day][currWorker] || count(sched[day].begin(), sched[day].end(), currWorker) > 1) {
+    return false;
+  }
+  return true;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,28 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void genRequirements(
+    std::set<std::string>& ret,
+    const std::string& options,
+    std::string input,
+    std::set<int> visited,
+    unsigned int index
+);
+
+void genAllStrs(
+    const std::string& floating,
+    std::set<std::string>& ret,
+    std::string input,
+    unsigned int index
+);
+
+void getInitStr(
+    std::string& options,
+    const std::string& in,
+    unsigned int remaining,
+    unsigned int index
+);
+
 
 
 // Definition of primary wordle function
@@ -22,7 +44,102 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+  std::string options = floating;
 
+  unsigned int emptyCount = 0;
+  for (unsigned int i = 0; i < in.size(); ++i) {
+    if (!isalpha(in[i])) {
+      emptyCount++;
+    }
+  }
+  unsigned int remaining = emptyCount - floating.size();
+  
+  getInitStr(options, in, remaining, 0);
+  
+  std::set<std::string> init;
+  std::set<int> visited; //to generate all combos with no reps
+  genRequirements(init, options, in, visited, 0);
+  
+  std::set<std::string>::iterator it = init.begin();
+  std::set<std::string> ret;
+  while (it != init.end()) {
+    genAllStrs(floating, ret, *it, 0);
+    it++;
+  }
+
+  std::set<std::string>::iterator it2 = ret.begin();
+  while (it2 != ret.end()) {
+    if (dict.find(*it2) == dict.end()) {
+      it2 = ret.erase(it2);
+    }
+    else {
+      it2++;
+    }
+  }
+
+  return ret;
 }
 
 // Define any helper functions here
+void genRequirements(
+    std::set<std::string>& ret,
+    const std::string& options,
+    std::string input,
+    std::set<int> visited,
+    unsigned int index
+)   
+{
+  if (index == input.size() && ret.find(input) == ret.end()) {
+    ret.insert(input);
+  }
+  else if (isalpha(input[index])) { //if we have a locked-in letter
+    genRequirements(ret, options, input, visited, index+1);
+  }
+  else {
+    for (unsigned int i = 0; i < options.size(); ++i) {
+      if (visited.find(i) != visited.end()) { //if we've seen this floating number before
+        continue;
+      }
+      visited.insert(i);
+      input[index] = options[i];
+      genRequirements(ret, options, input, visited, index+1);
+      visited.erase(i);
+    }
+  }
+}
+
+void genAllStrs(
+    const std::string& floating,
+    std::set<std::string>& ret,
+    std::string input,
+    unsigned int index
+) 
+{
+  if (index == input.size()) {
+    ret.insert(input);
+  }
+  else if (isalpha(input[index])) {
+    genAllStrs(floating, ret, input, index+1);
+  }
+  else {
+    for (int i = 0; i < 26; ++i) {
+      input[index] = char('a'+i);
+      genAllStrs(floating, ret, input, index+1);
+    }
+  }
+}
+
+void getInitStr(
+    std::string& options,
+    const std::string& in,
+    unsigned int remaining,
+    unsigned int index
+) 
+{
+  if (index == remaining) {
+    return;
+  }
+  options+='_';
+  getInitStr(options, in, remaining, index+1);
+}
+
